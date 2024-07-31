6f0164a6cf2aff49e872c06c92811aeeb601f55a
Alfonso Nieto García <43706214+ANG112@users.noreply.github.com>
2024-07-31 20:20:01 +0200

    Comprobación funcionamiento

--- app.py
+++ app.py
@@ -8,8 +8,9 @@ from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error
 import numpy as np
 
 # Path settings
-csv_path = '/data/revenue.csv'
-model_path = '/data/ad_model.pkl'
+csv_path = 'data/revenue.csv'
+csv_path_retrain = 'data/revenue_updated.csv'
+model_path = 'ad_model.pkl'
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -33,14 +34,14 @@ def predict():
         if followers is not None and average_views is not None and average_interactions is not None and posting_frequency is not None:
             prediction = model.predict([[float(followers), float(average_views), float(average_interactions), float(posting_frequency)]])[0]
 
-    return render_template("predict.html", prediction=prediction)
+    return render_template("predict.html", prediction=round(prediction,2))
 
 # Retrain route
 @app.route('/api/v1/retrain', methods=["GET"])
 def retrain():
     metrics = None
-    if os.path.exists(csv_path):
-        data = pd.read_csv(csv_path)
+    if os.path.exists(csv_path_retrain):
+        data = pd.read_csv(csv_path_retrain)
 
         # Using the new columns
         X_train, X_test, y_train, y_test = train_test_split(data.drop(columns=['revenue']),
@@ -50,13 +51,13 @@ def retrain():
 
         model = Lasso(alpha=6000)
         model.fit(X_train, y_train)
-        rmse = np.sqrt(mean_squared_error(y_test, model.predict(X_test)))
-        mape = mean_absolute_percentage_error(y_test, model.predict(X_test))
+        rmse = round(np.sqrt(mean_squared_error(y_test, model.predict(X_test))),2)
+        mape = round(mean_absolute_percentage_error(y_test, model.predict(X_test)),2)
         model.fit(data.drop(columns=['revenue']), data['revenue'])
         pickle.dump(model, open(model_path, 'wb'))
 
         metrics = {'rmse': rmse, 'mape': mape}
-
+        
     return render_template("retrain.html", metrics=metrics)
 
 if __name__ == '__main__':
