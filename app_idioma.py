c7032a2bab803c04d061c51fa07029f3a1145901
Brenda Rodriguez Farfan <brendarodriguezfarfan@gmail.com>
2024-08-09 21:05:44 +0200

    clase

--- app_idioma.py
+++ app_idioma.py
@@ -50,7 +50,7 @@ def retrain():
         X_train, X_test, y_train, y_test = train_test_split(data.drop(columns=['revenue']),
                                                             data['revenue'],
                                                             test_size=0.20,
-                                                            random_state=42)
+                                                            random_state=43)
 
         model = Lasso(alpha=6000)
         model.fit(X_train, y_train)
